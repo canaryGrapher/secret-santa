06eb3beadfb35a6a62f18513fa6d447f7a224b99
canaryGrapher <yasharyan307@outlook.com>
2022-12-20 23:27:56 +0530

    Removed dist folder

--- dist/routes/intimate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const starterRouter = (0, express_1.Router)();
-starterRouter.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-exports.default = starterRouter;
